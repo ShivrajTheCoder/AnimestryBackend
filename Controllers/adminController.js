627a0619f7da963ea0f2aa2859c1a6e66440df3f
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-03-10 00:31:14 +0530

    admin-auth-added

--- Controllers/adminController.js
+++ Controllers/adminController.js
@@ -5,7 +5,10 @@ const OrderModel = require("../Models/OrderModel");
 const Order=require("../Models/OrderModel");
 const Products=require("../Models/ProductModel");
 const Users=require("../Models/UserModel");
+const Admin=require("../Models/AdminModel");
 const { NotFoundError } = require("../Utilities/CustomErrors");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const exp=module.exports;
 
 exp.Dashboardinfo=RouterAsncErrorHandler(async(req,res,next)=>{
@@ -47,5 +50,60 @@ exp.MarkAsDeliverd=RouterAsncErrorHandler(async(req,res,next)=>{
 
     } catch (error) {
         next(error)
+    }
+})
+const USER_KEY=process.env.JWT_SECRET_KEY_ADMIN;
+exp.Login = async (req, res, next) => {
+    const { email, password } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+        const user = await Admin.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: "Incorrect email or password." });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({ message: "Incorrect email or password." });
+        }
+        const token = jwt.sign({ _id: user._id }, USER_KEY);
+
+        res.status(200).json({ message: "Login successful", token, userId: user._id });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Error logging in." });
+    }
+};
+
+const saltRounds=10;
+exp.Signup=RouterAsncErrorHandler(async(req,res,next)=>{
+    const {name,email,password}=req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+    try {
+        if (!name || !email || !password) {
+            return res.status(400).json({ message: 'Missing required fields: name, email, password, and verification code.' });
+        }
+
+        const user = await Admin.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: 'User Already Exists' });
+        }
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+        const newUser = new Admin({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await newUser.save();
+        res.status(201).json({ message: 'User created successfully.', user: savedUser.email });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error creating user.' });
     }
 })
--- Routes/adminRoutes.js
+++ Routes/adminRoutes.js
@@ -1,12 +1,26 @@
 const express=require("express");
 const router=express.Router();
-const {Dashboardinfo, MarkAsDeliverd} =require("../Controllers/adminController");
+const {Dashboardinfo, MarkAsDeliverd, Signup, Login} =require("../Controllers/adminController");
 const { check } = require("express-validator");
 router.route("/dashinfo")
     .get(Dashboardinfo)
+
+router.route("/login")
+    .post([
+        check("email").exists().isEmail(),
+        check("password").exists().isLength({ min: 5 }),
+    ],Login)
+
+router.route("/signup")
+    .post([
+        check("name").exists(),
+        check("email").exists().isEmail(),
+        check("password").exists().isLength({ min: 5 }),
+    ],Signup)
 
 router.route("/markasdelivered/:orderId")
     .put([
         check("orderId").exists().isMongoId(),
     ],MarkAsDeliverd)
+
 module.exports=router;
