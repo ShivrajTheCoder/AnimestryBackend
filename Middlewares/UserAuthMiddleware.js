7b1b3f350a61ff0181e0b468c6a62f342708d1e4
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-03 12:41:43 +0530

    comments-removed

--- Middlewares/UserAuthMiddleware.js
+++ Middlewares/UserAuthMiddleware.js
@@ -1,20 +1,19 @@
 const jwt = require('jsonwebtoken');
 
 const authenticateToken = (req, res, next) => {
-
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; 
+    const token = authHeader && authHeader.split(' ')[1];
+    // console.log(token, authHeader); 
     if (!token) {
         return res.sendStatus(401);
     }
-    console.log(token);
+    // console.log(token, "here is the token");
     // Verify the JWT token
     jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
         if (err) {
             return res.sendStatus(403);
         }
         req.user = user;
-        console.log(user);
         next();
     });
 };
