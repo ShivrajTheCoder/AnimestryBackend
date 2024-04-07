aeacae277f3c884a707b3fee90f091bcb3043f26
ShivrajTheCoder <thapliyalshivraj@gmail.com>
2024-04-07 10:14:34 +0530

    remove-from-file-solved

--- Utilities/RemoveFileFromLocal.js
+++ Utilities/RemoveFileFromLocal.js
@@ -1,10 +1,12 @@
+const fs = require('fs');
+
 const removeFileFromLocal = (image, otherimages) => {
     fs.unlinkSync(image[0].path);
-        if (otherimages?.length > 0) {
-            otherimages.forEach((otherimage) => {
-                fs.unlinkSync(otherimage.path);
-            });
-        }
+    if (otherimages?.length > 0) {
+        otherimages.forEach((otherimage) => {
+            fs.unlinkSync(otherimage.path);
+        });
+    }
 }
 
-module.exports=removeFileFromLocal;
+module.exports = removeFileFromLocal;
